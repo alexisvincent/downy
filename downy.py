9a24113d750ec6d3d5b6ab3a18b68128708c5ee5
Jacob Lee <artdent@gmail.com>
2009-05-22 18:27:23 -0500

    Added robot middleware

--- downy.py
+++ downy.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python2.4
 
+import logging
+
 from api import robot
 from api import events
 
 
+class RobotMiddleware(object):
+  def __init__(self, robot_app, main_app):
+    self._robot = robot_app
+    self._main = main_app
+
+  def __call__(self, environ, start_response):
+    path = env['PATH_INFO']
+    if path.startswith('_wave/'):
+      return self._robot(environ, start_response)
+    return self._main(environ, start_response)
+
+
 class Downy(object):
+  def __init__(self):
+    self._announced = False
+
   def on_blip_created(self, properties, context):
-    print 'blip created'
-    print properties
-    print context
+    logging.info('blip created')
+    logging.info(properties)
+    logging.info(context)
 
   def on_participants_changed(self, properties, context):
-    print 'somebody was added'
-    print properties['participantsAdded']
-    #if self._in_participant_list(properties['participantsAdded']):
-    #
+    if self._announced:
+      return
+    logging.info(properties['participantsAdded'])
+    if self._in_participant_list(properties['participantsAdded']):
+      self.announce()
+
+  def announce(self):
+    pass
+
+  def _in_participant_list(self, l):
+    pass
 
 
 if __name__=='__main__':
