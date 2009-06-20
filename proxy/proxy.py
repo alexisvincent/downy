d9460a4e9eb07fb8a5ed806337132ae018f74a5f
Jacob Lee <artdent@gmail.com>
2009-06-19 23:50:02 -0500

    Use webapp framework for proxy

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,21 +1,32 @@
+import logging
+
 from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
 
 
-REMOTE_ROBOT = 'http://artdent-chi.homelinux.net/'
+REMOTE_ROBOT = 'http://artdent-chi.homelinux.net'
 
 
-def proxy(environ, start_response):
-  robot_response = urlfetch.fetch(
-      REMOTE_ROBOT + environ['PATH_INFO'], method=environ['REQUEST_METHOD'])
-  resp.set_status(robot_response.status_code)
-  resp.out.write(robot_response.content)
-  resp.wsgi_write(start_response)
+class Proxy(webapp.RequestHandler):
+
+  def _forward(self, method):
+    path = REMOTE_ROBOT + self.request.path
+    logging.info('path: <%s>', path)
+    robot_response = urlfetch.fetch(path, method=method, deadline=10)
+    logging.info('remote response: %d', robot_response.status_code)
+    self.response.set_status(robot_response.status_code)
+    self.response.out.write(robot_response.content)
+
+  def get(self):
+    self._forward('GET')
+
+  def post(self):
+    self._forward('POST')
 
 
 def main():
-  webapp.util.run_wsgi_app(proxy)
+  app = webapp.WSGIApplication([('.*', Proxy)], debug=True)
+  webapp.util.run_wsgi_app(app)
 
 
 if __name__=='__main__':
