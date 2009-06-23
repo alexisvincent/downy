211cdcc7894abb299df909eac9d8326067bd0e58
jacob <jacob@atuttle>
2009-06-23 01:22:29 -0500

    proxy: Retry upon DownloadError (not that it helps)

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -2,18 +2,29 @@ import logging
 
 from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
+import google.appengine.ext.webapp.util
 
 
 REMOTE_ROBOT = 'http://artdent-chi.homelinux.net'
-
+ATTEMPTS = 3
 
 class Proxy(webapp.RequestHandler):
 
   def _forward(self, method):
     path = REMOTE_ROBOT + self.request.path
     logging.info('path: <%s>', path)
-    robot_response = urlfetch.fetch(path, method=method, deadline=10)
-    logging.info('remote response: %d', robot_response.status_code)
+    robot_response = None
+    for i in range(ATTEMPTS):
+      try:
+        robot_response = urlfetch.fetch(path, method=method, deadline=10)
+        logging.info('remote response: %d', robot_response.status_code)
+      except urlfetch.DownloadError, e:
+        logging.error('Download error: %s', e)
+    if not robot_response:
+      raise RuntimeError(
+        'Did not receive response from %s after %s tries. '
+        'Either the host is down or urlfetch is still broken.'
+        % (path, ATTEMPTS))
     self.response.set_status(robot_response.status_code)
     self.response.out.write(robot_response.content)
 
