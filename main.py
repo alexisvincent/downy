7f64b1a85ec71676fb35506da2ee0b1771d03038
Jacob Lee <artdent@gmail.com>
2009-06-26 16:17:41 -0500

    Correctly license under the Apache 2.0 license.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python2.5
+#
+# Copyright 2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """Main entry point for Downy."""
 
@@ -26,7 +40,7 @@ def downy_app(repo_path):
 
   model = downy.Downy(repo)
   bot = robot_abstract.Robot(
-      'Downy', version=1,
+      'Downy', version='1',
       image_url='http://downybot.appspot.com/public/downy.png',
       profile_url='http://downybot.appspot.com/public/profile.xml')
   bot.RegisterListener(model)
